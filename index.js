0e0ac8ed520a6285b9d8370778a1031f8b7ead7d
tat-takaha <tat641ta@gmail.com>
2018-04-07 11:42:22 +0900

    commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// node.js v8以降で動作します。
+const lib = require('./lib/sample-lib.js')
+exports.hi 	= (name) => "やあ！"+ name
+
+exports.hello 		= (name) => lib.hello(name)                                             // sample-lib.jsの関数を利用している
+exports.helloSum 	= (name , ...arg) => `${lib.hello(name)}。合計は${lib.sum(...arg)}です。`   // sample-lib.jsの関数を利用している
---
+++ test/test.js
@@ -0,0 +1,19 @@
+// node.js v8以降で動作します。
+const assert   = require('power-assert')  // power-assertをインポート
+const myModule = require('../index')      // プログラム本体である、index.jsを読み込む
+// テストコード
+describe('作ったプログラムを次の項目ごとにテストします' , () => {
+
+        it('「やあ！」と挨拶しないとだめ', () => {
+            //assert.equal(myModule.hi('太郎'), 'やあ！太郎')
+            assert.equal(myModule.hi('太郎'), 'やあ！太郎')
+        })
+
+        /*it('「ハロー！」と挨拶しないとだめ', () => {
+            assert.equal(myModule.hello('太郎'), 'はろー！太郎')
+        })
+        it('挨拶と合計を計算', () => {
+            assert.equal(myModule.helloSum('太郎',1,2,3,4), 'はろー！太郎。合計は10です。')
+        })
+        */
+})
