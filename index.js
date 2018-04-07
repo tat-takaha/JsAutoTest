91bd2f6ae2c45e7688d6a7d152c18e3af3205f4e
tat-takaha <tat641ta@gmail.com>
2018-04-07 14:56:49 +0900

    ファイル更新

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const lib = require('./lib/sample-lib.js')
 exports.hi 	= (name) => "やあ！"+ name
 
 exports.hello 		= (name) => lib.hello(name)                                             // sample-lib.jsの関数を利用している
-exports.helloSum 	= (name , ...arg) => `${lib.hello(name)}。合計は${lib.sum(...arg)}だす。`   // sample-lib.jsの関数を利用している
+exports.helloSum 	= (name , ...arg) => `${lib.hello(name)}。合計は${lib.sum(...arg)}です。`   // sample-lib.jsの関数を利用している
