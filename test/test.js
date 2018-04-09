70ef17d67e62a8884978b48256feed64a8d316e2
tat-takaha <tat641ta@gmail.com>
2018-04-09 10:22:08 +0900

    update

--- test/test.js
+++ test/test.js
@@ -9,10 +9,10 @@ describe('作ったプログラムを次の項目ごとにテストします' , 
             assert.equal(myModule.hi('太郎'), 'やあ！太郎')
         })
 
-        /*it('「ハロー！」と挨拶しないとだめ', () => {
+        it('「ハロー！」と挨拶しないとだめ', () => {
             assert.equal(myModule.hello('太郎'), 'はろー！太郎')
         })
-        */
+        
         it('挨拶と合計を計算', () => {
             assert.equal(myModule.helloSum('太郎',1,2,3,4), 'はろー！太郎。合計は10です。')
         })
